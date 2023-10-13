27207a514773bb98ae2719d615291f70e21909c9
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-10-13 17:37:07 +0900

    [FE] Init: frontend init default project - Next.js 프로젝트 생성 - storybook 적용 - 현재 PWA 적용 작업 중

---
+++ front/next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public",
+        },
+      },
+    ],
+  ],
+  nextConfig
+);

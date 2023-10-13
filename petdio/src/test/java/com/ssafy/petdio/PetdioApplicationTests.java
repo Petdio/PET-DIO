834948b4d058053ed615671ca17b14114c5c5f7b
mj <aud19980518@gmail.com>
2023-10-13 15:07:10 +0900

    Init: backend init default project
    Init: backend init default project

---
+++ petdio/src/test/java/com/ssafy/petdio/PetdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.petdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

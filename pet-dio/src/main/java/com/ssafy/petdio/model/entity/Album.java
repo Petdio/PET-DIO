1851d69689a196f4f55ac946a1915b33fbb8ff86
chlgnlqls3 <chlgnlqls3@nate.com>
2023-10-16 17:12:12 +0900

    feat:[BE] Album 조회 api 제작 중

---
+++ pet-dio/src/main/java/com/ssafy/petdio/model/entity/Album.java
@@ -0,0 +1,36 @@
+package com.ssafy.petdio.model.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+
+@Entity
+@Getter
+@Setter
+public class Album {
+
+    @Id
+    @GeneratedValue
+    private Long album_id;
+
+    @OneToMany(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User album_user_id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "concept_id")
+    private Concept album_concept_id;
+
+    private String album_imgURL;
+
+    private ZonedDateTime createdat;
+
+}

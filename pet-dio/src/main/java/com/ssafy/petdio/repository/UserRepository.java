12071088e56590561ce3485f76cdd1cc828e3eb4
novventa <novventa@kakao.com>
2023-10-17 14:16:17 +0900

    [BE] Feat: UserRepository 추가

---
+++ pet-dio/src/main/java/com/ssafy/petdio/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.ssafy.petdio.repository;
+
+import com.ssafy.petdio.model.Enum.SocialType;
+import com.ssafy.petdio.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, Integer> {
+
+    Optional<User> findByIdAndIsDeleted(int id, int isDeleted);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findBySocialTyoeAndSocialId(SocialType socialType, String socialId);
+
+}

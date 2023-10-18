b5f0a255b03c1f0dd6cef0b8450f95c7125de79f
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-10-18 16:00:00 +0900

    [FE] Fix: MUI 적용 후 발생한 서비스워커 관련 이슈 해결
    - runtimeCaching을 next.config.js에 추가함으로써 해결
    - 위 사항 때문에 run dev 실행 시 터보팩 사용 못하도록 수정함

--- front/next.config.js
+++ front/next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
-
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
 });
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   reactStrictMode: true,
+  runtimeCaching,
 });

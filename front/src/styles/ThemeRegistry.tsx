995c7a6dc50f02c71ab6cdd0748ecfd0df1fceb0
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-10-18 17:40:58 +0900

    [FE] Fix: 폰트 적용 이슈 해결
    - 홈페이지 스토리북에서 조회 시 폰트 적용 안되는 문제 해결
    - mui에서 폰트 적용하는 방법 알아냄

--- front/src/styles/ThemeRegistry.tsx
+++ front/src/styles/ThemeRegistry.tsx
@@ -1,37 +1,37 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeOptions, ThemeProvider } from '@mui/material/styles';
-import { NextAppDirEmotionCacheProvider } from './EmotionCache';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeOptions, ThemeProvider } from "@mui/material/styles";
+import { NextAppDirEmotionCacheProvider } from "./EmotionCache";
 
-/** 여기서 앱 테마를 설정하면 된다 */
+/** 여기서 MUI 테마를 설정하면 된다 */
 
 const themeOptions: ThemeOptions = {
   typography: {
     fontSize: 16,
-    fontFamily: 'Pretendard',
+    fontFamily: "Pretendard",
   },
   palette: {
     background: {
-      default: 'dark',
+      default: "dark",
     },
     primary: {
-      main: '#8758FF',
-      light: '#A185FF',
-      dark: '#7430F7',
-      contrastText: '#fff',
+      main: "#8758FF",
+      light: "#A185FF",
+      dark: "#7430F7",
+      contrastText: "#fff",
     },
     secondary: {
-      main: '#5CB8E4',
-      light: '#8CCCED',
-      dark: '#2798D0',
-      contrastText: '#fff',
+      main: "#5CB8E4",
+      light: "#8CCCED",
+      dark: "#2798D0",
+      contrastText: "#fff",
     },
     text: {
-      primary: '#18181887',
-      secondary: '#18181860',
-      disabled: '#18181838',
+      primary: "#18181887",
+      secondary: "#18181860",
+      disabled: "#18181838",
     },
   },
 };
@@ -44,7 +44,7 @@ export default function ThemeRegistry({
   children: React.ReactNode;
 }) {
   return (
-    <NextAppDirEmotionCacheProvider options={{ key: 'mui' }}>
+    <NextAppDirEmotionCacheProvider options={{ key: "mui" }}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         {children}

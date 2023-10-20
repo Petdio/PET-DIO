6e1eb195c2644a0d55a80708d1eb2a8f638b898b
serenecarp <spmjoon516@gmail.com>
2023-10-20 09:32:28 +0900

    [FE] Feat: 테마 선택 카드

--- front/src/components/studio/theme-card/ThemeCard.tsx
+++ front/src/components/studio/theme-card/ThemeCard.tsx
@@ -1,5 +1,39 @@
-function ThemeCard() {
-  return 0;
+import Image from 'next/image';
+import { Typography, Box } from '@mui/material';
+
+interface Props {
+  imgSrc: string;
+  themeName: string;
+  onClick?: () => {};
+}
+
+function ThemeCard({ imgSrc, themeName }: Props) {
+  return (
+    <div>
+      <Box
+        sx={{
+          position: 'relative',
+          marginBottom: '0.25rem',
+          aspectRatio: 1 / 1,
+          minWidth: 150,
+          maxWidth: 200,
+        }}
+      >
+        <Image
+          src={imgSrc}
+          alt={themeName}
+          fill
+          objectFit="cover"
+          objectPosition="center center"
+          placeholder="empty"
+          style={{ borderRadius: '0.5rem' }}
+        />
+      </Box>
+      <div>
+        <Typography fontWeight={700}>{themeName}</Typography>
+      </div>
+    </div>
+  );
 }
 
 export default ThemeCard;

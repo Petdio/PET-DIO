e67bef791f7ab524a71140d4c1715d223b7cc913
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-10-22 18:43:13 +0900

    [FE] Feat: 로딩 페이지 스토리북 업로드

---
+++ front/src/app/loading/Loading.stories.tsx
@@ -0,0 +1,13 @@
+import Loading from "./page";
+import RootLayout from "../layout";
+
+export default {
+  title: "Pages/Loading",
+  component: Loading,
+};
+
+export const LoadingPage = () => (
+  <RootLayout>
+    <Loading />
+  </RootLayout>
+);

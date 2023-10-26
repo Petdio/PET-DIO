9620a931deff26cf491a766663afc0eac37335b5
Harvey Kwak <harvey970121@naver.com>
2023-10-26 15:16:14 +0900

    [infra] Feat: AwsConfig 작성

--- back/src/main/java/com/ssafy/petdio/config/AwsConfig.java
+++ back/src/main/java/com/ssafy/petdio/config/AwsConfig.java
@@ -1,11 +1,12 @@
 package com.ssafy.petdio.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.beans.BeanProperty;
-
 @Configuration
 public class AwsConfig {
 
@@ -21,7 +22,12 @@ public class AwsConfig {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(S3AccessKey, S3SecretKey);
+        BasicAWSCredentials credentials = new BasicAWSCredentials(S3AccessKey, S3SecretKey);
 
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
     }
 }

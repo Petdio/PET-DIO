6b3d63fe9b523e698079198654c467bf64aa94e5
mj <aud19980518@gmail.com>
2023-10-26 15:08:01 +0900

    [BE] Feat: CRUD 리팩토링 및 에러 핸들링
    [BE] Feat: CRUD 리팩토링 및 에러 핸들링

---
+++ back/src/main/generated/com/ssafy/petdio/model/mapper/UserMapperImpl.java
@@ -0,0 +1,29 @@
+package com.ssafy.petdio.model.mapper;
+
+import com.ssafy.petdio.model.dto.UserDto;
+import com.ssafy.petdio.model.entity.User;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-10-26T15:03:04+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.7 (Eclipse Adoptium)"
+)
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDto entityToUserDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        userDto.setUserId( user.getUserId() );
+        userDto.setUserNickname( user.getUserNickname() );
+        userDto.setProfileImage( user.getProfileImage() );
+        userDto.setRole( user.getRole() );
+
+        return userDto;
+    }
+}

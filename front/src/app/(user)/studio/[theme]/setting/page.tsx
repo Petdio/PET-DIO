20ccea48d7b66f8cbc1d304838f47ba816fa61c8
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-10-27 15:17:10 +0900

    [FE] Feat: AI 설정 페이지 제작중

--- front/src/app/(user)/studio/[theme]/setting/page.tsx
+++ front/src/app/(user)/studio/[theme]/setting/page.tsx
@@ -10,6 +10,12 @@ export default function Setting() {
   const [animalSelected, setAnimalSelected] = useState(false);
   const [animalIdx, setAnimalIdx] = useState(-1);
 
+  const animalType = ["개", "고양이"];
+  const animalLabelSet = [
+    { label: "견종", comment: "반려견의 견종을 선택해주세요." },
+    { label: "묘종", comment: "반려묘의 묘종을 선택해주세요." },
+  ];
+
   const onSelect = (idx: number) => {
     setAnimalSelected(true);
     setAnimalIdx(idx);
@@ -18,19 +24,20 @@ export default function Setting() {
   return (
     <>
       <Subtitle content="어떤 동물의 사진인가요?" />
-      <AnimalSelectRadioGroup
-        animalItems={["개", "고양이"]}
-        onSelect={onSelect}
-      />
+      <AnimalSelectRadioGroup animalItems={animalType} onSelect={onSelect} />
       {animalSelected && (
         <Box sx={{ width: "100%", padding: "1rem" }}>
           <Autocomplete
             disablePortal
             id="dog-breed-selection"
             options={dogBreedList}
-            renderInput={(params) => <TextField {...params} label="견종" />}
+            renderInput={(params) => (
+              <TextField {...params} label={animalLabelSet[animalIdx].label} />
+            )}
           />
-          <Typography color="grey">반려견의 견종을 선택해주세요.</Typography>
+          <Typography color="grey">
+            {animalLabelSet[animalIdx].comment}
+          </Typography>
         </Box>
       )}
     </>

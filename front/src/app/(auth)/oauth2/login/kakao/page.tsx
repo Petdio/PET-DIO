c38e17c9a21a7eccd56b55a37ed154c46c94f6d6
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-10-30 10:58:38 +0900

    [FE] Refactor: 첫 페이지 설정 및 로그인 후 경로 수정

--- front/src/app/(auth)/oauth2/login/kakao/page.tsx
+++ front/src/app/(auth)/oauth2/login/kakao/page.tsx
@@ -1,25 +1,29 @@
 "use client";
 import { useEffect } from "react";
 import axios from "axios";
+import { TrySharp } from "@mui/icons-material";
 
 export default function KakaoLogInPage() {
+  async function loginReq(code: any) {
+    try {
+      const response = await axios.post(
+        `http://k9a206.p.ssafy.io:8080/oauth2/login/kakao`,
+        { code }
+      );
+
+      console.log(response);
+      localStorage.setItem("access-token", response.data.accessToken);
+      window.location.href = "/studio";
+    } catch (error) {
+      console.error("에러 발생:", error);
+    }
+  }
+
   useEffect(() => {
     const code = new URL(document.location.toString()).searchParams.get("code");
     console.log("code", code);
     if (code) {
-      axios({
-        method: "post",
-        url: `http://k9a206.p.ssafy.io:8080/oauth2/login/kakao`,
-        data: { code },
-      })
-        .then((res) => {
-          console.log(res);
-          localStorage.setItem("access-token", res.data.accessToken);
-          window.location.href = "/";
-        })
-        .catch((error) => {
-          console.error("에러 발생:", error);
-        });
+      loginReq(code);
     }
   }, []);
 

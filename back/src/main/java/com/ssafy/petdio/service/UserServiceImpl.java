d92e3d2ca28e402cbccfc789044eeccf30221c04
novventa <novventa@kakao.com>
2023-10-30 16:18:51 +0900

    [BE] Fix: User Profile 가져오기 버그 수정

--- back/src/main/java/com/ssafy/petdio/service/UserServiceImpl.java
+++ back/src/main/java/com/ssafy/petdio/service/UserServiceImpl.java
@@ -59,7 +59,7 @@ public class UserServiceImpl implements UserService {
 
     // userId로 유저를 찾고, 없으면 throw Exception
     private User findUserById(Long id) {
-        return userRepository.findByUserIdAndUserDeleteFalse(id)
+        return userRepository.findByUserIdAndUserDeleteIsNull(id)
                 .orElseThrow(() -> new RuntimeException("해당하는 유저를 찾을 수 없습니다"));
     }
 

4312a127ccdd3293447d618ee3ef6efe691b2ccf
serenecarp <spmjoon516@gmail.com>
2023-10-30 16:52:11 +0900

    [FE] Refactor: 앨범 필터 조건문 간소화

--- front/src/components/album/album-list/AlbumList.tsx
+++ front/src/components/album/album-list/AlbumList.tsx
@@ -7,7 +7,6 @@ import ThemeSection from '@/components/album/theme-section/ThemeSection';
 import ThemeSelectButton from '@/components/album/theme-select/theme-select-button/ThemeSelectButton';
 import ThemeSelectBottomSheet from '@/components/album/theme-select/theme-select-bottom-sheet/ThemeSelectBottomSheet';
 import DetailModal from '@/components/album/detail/detail-modal/DetailModal';
-import PageTitle from '@/components/common/page-title/PageTitle';
 // interfaces
 import { ModalInfoProps } from '@/interfaces/ModalInfoProps';
 import { AlbumDataProps } from '@/interfaces/AlbumDataProps';
@@ -18,24 +17,24 @@ interface Props {
 }
 
 function AlbumList({ dummy }: Props) {
+  const dummyData = dummy;
   const dummyTheme = dummy.map((data) => {
     const key = Object.keys(data);
     return key[0];
   });
-  const dummyData = dummy;
   const themeNames = dummyTheme;
+  
   const [filterOpen, setFilterOpen] = useState(false);
   const handleFilterToggle = (open: boolean) => {
     setFilterOpen(open);
   };
-
   const [filteredThemeIdx, setFilteredThemeIdx] = useState(-1);
   const handleFilterIdx = (idx: number) => {
     setFilteredThemeIdx(idx);
     setFilterOpen(false);
   };
-  const [modalOpen, setModalOpen] = useState(false);
 
+  const [modalOpen, setModalOpen] = useState(false);
   const [modalInfo, setModalInfo] = useState<ModalInfoProps>({
     imgSrc: '',
     themeName: '',
@@ -51,46 +50,28 @@ function AlbumList({ dummy }: Props) {
 
   return (
     <>
-      {/** @todo 둘을 하나로 통합할 것 */}
-      {filteredThemeIdx === -1 ? (
-        <Box marginTop="1rem">
-          {dummyData.map((data, idx) => {
-            const themeName = Object.keys(data)[0];
-            return (
-              <Box
-                key={idx}
-                display="block"
-              >
-                <ThemeSection
-                  themeName={themeName}
-                  imgList={data[themeName]}
-                  onClickFn={handleModalOpen}
-                />
-                <Box height="1rem" />
-              </Box>
-            );
-          })}
-        </Box>
-      ) : (
-        <Box marginTop="1rem">
-          {dummyData.map((data, idx) => {
-            const themeName = Object.keys(data)[0];
-            return (
-              <Box
-                key={idx}
-                display={idx === filteredThemeIdx ? 'block' : 'none'}
-              >
-                <ThemeSection
-                  themeName={themeName}
-                  imgList={data[themeName]}
-                  onClickFn={handleModalOpen}
-                />
-                <Box height="1rem" />
-              </Box>
-            );
-          })}
-        </Box>
-      )}
+      <Box marginTop="1rem">
+        {dummyData.map((data, idx) => {
+          const themeName = Object.keys(data)[0];
+          const isDisplayed =
+            filteredThemeIdx === -1 || filteredThemeIdx === idx;
+
+          return (
+            <Box
+              key={idx}
+              display={isDisplayed ? 'block' : 'none'}
+            >
+              <ThemeSection
+                themeName={themeName}
+                imgList={data[themeName]}
+                onClickFn={handleModalOpen}
+              />
+              <Box height="1rem" />
+            </Box>
+          );
+        })}
+      </Box>
+
       <DetailModal
         isOpen={modalOpen}
         themeName={modalInfo.themeName}

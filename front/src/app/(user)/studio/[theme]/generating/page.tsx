7a320432a721649025786be490aa148d5687f8c0
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-10-30 16:22:51 +0900

    [FE] Feat: Generating 페이지 컴포넌트 전환 및 페이지 이동 추가

--- front/src/app/(user)/studio/[theme]/generating/page.tsx
+++ front/src/app/(user)/studio/[theme]/generating/page.tsx
@@ -1,6 +1,28 @@
+"use client";
+import { useState, useEffect } from "react";
 import { LinearProgress, Box, Typography } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import { useRouter } from "next/navigation";
 
 export default function Generating() {
+  const router = useRouter();
+  const [showComponent1, setShowComponent1] = useState(true);
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      setShowComponent1(false);
+    }, 3000);
+
+    const timer2 = setTimeout(() => {
+      router.push("result");
+    }, 4000);
+
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, [router]);
+
   return (
     <Box
       sx={{
@@ -16,14 +38,29 @@ export default function Generating() {
           justifyContent: "space-around",
         }}
       >
-        <Typography
-          variant="body1"
-          color="black"
-          sx={{ textAlign: "center", mb: "20px" }}
-        >
-          댕댕이 옷 입히는 중...
-        </Typography>
-        <LinearProgress />
+        {showComponent1 ? (
+          <>
+            <Typography
+              variant="body1"
+              color="black"
+              sx={{ textAlign: "center", mb: "20px" }}
+            >
+              댕댕이 옷 입히는 중...
+            </Typography>
+            <LinearProgress />
+          </>
+        ) : (
+          <>
+            <Typography
+              variant="body1"
+              color="black"
+              sx={{ textAlign: "center", mb: "20px" }}
+            >
+              이미지 생성 완료!
+            </Typography>
+            <CheckIcon color="primary" sx={{ width: "100%" }} />
+          </>
+        )}
       </Box>
     </Box>
   );

330f4c87d114d6615a2ae14235f7efb5416a5936
mj <aud19980518@gmail.com>
2023-10-31 14:16:19 +0900

    [BE] Feat: 레오나르도 코드 api 수정
    [BE] Feat: 레오나르도 코드 api 수정

--- back/src/main/java/com/ssafy/petdio/controller/ConceptController.java
+++ back/src/main/java/com/ssafy/petdio/controller/ConceptController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,10 +37,11 @@ public class ConceptController {
     }
 
     @PostMapping("/image")
-    public String makeAiImage(@RequestParam(value = "file", required = false) MultipartFile file, Authentication authentication) {
+    public String makeAiImage(@RequestParam(value = "file", required = false) MultipartFile file, @RequestParam(value = "conceptId") Long conceptId, Authentication authentication) {
         log.info("make Ai Image");
+        System.out.println("userId: " + Long.parseLong(authentication.getName()));
         try {
-            aiService.makeAiImage(Long.parseLong(authentication.getName()), file);
+            aiService.makeAiImage(conceptId, file);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }

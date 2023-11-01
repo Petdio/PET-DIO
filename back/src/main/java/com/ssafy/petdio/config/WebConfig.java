7f344e22e126d15c1e18c80383d4ed1efee15a87
mj <aud19980518@gmail.com>
2023-11-01 14:08:44 +0900

    [BE] Feat: 로컬 cors에러
    [BE] Feat: 로컬 cors에러

---
+++ back/src/main/java/com/ssafy/petdio/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ssafy.petdio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")      //패턴
+                .allowedOrigins("*")    //URL
+                .allowedMethods("GET","POST","PUT","DELETE");        //method
+    }
+}

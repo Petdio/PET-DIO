2a41a53b07de406211df33c7f2125647ec94853d
mj <aud19980518@gmail.com>
2023-11-01 09:54:32 +0900

    [BE] Feat: 코드 수정
    [BE] Feat: 코드 수정

--- back/src/main/java/com/ssafy/petdio/auth/controller/AuthController.java
+++ back/src/main/java/com/ssafy/petdio/auth/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/oauth2")
 @RequiredArgsConstructor
 public class AuthController {

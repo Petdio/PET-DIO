e55bdb2558e66c2224b35b41fc15d97fc59377a7
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-11-02 17:48:47 +0900

    [FE] Feat: Firebase FCM 3차 테스트
    - 서비스 워커 수동 등록 코드 추가

--- front/src/components/FirebaseTest.tsx
+++ front/src/components/FirebaseTest.tsx
@@ -48,6 +48,16 @@ export default function FirebaseTest() {
   };
 
   useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("../../public/firebase-messaging-sw.js")
+        .then(function (registration) {
+          console.log("Registration successful, scope is:", registration.scope);
+        })
+        .catch(function (err) {
+          console.log("Service worker registration failed, error:", err);
+        });
+    }
     onMessageFCM();
   }, []);
 

8c275425dc198bdc2aa2ca26c4455e67164daf44
successboy <chlgnlqls3@nate.com>
2023-11-03 15:48:33 +0900

    feat: modelId 배열화

--- back/src/main/java/com/ssafy/petdio/service/AiServiceImpl.java
+++ back/src/main/java/com/ssafy/petdio/service/AiServiceImpl.java
@@ -9,14 +9,16 @@ import com.ssafy.petdio.model.entity.User;
 import com.ssafy.petdio.repository.AlbumRepository;
 import com.ssafy.petdio.repository.SettingRepository;
 import com.ssafy.petdio.util.Leonardo;
-import java.io.IOException;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 @Service
@@ -36,8 +38,20 @@ public class AiServiceImpl implements AiService {
     @Override
     public void makeAiImage(Long conceptId, MultipartFile multipartFile, String breed, Long userId) throws IOException {
         List<Setting> settings = settingRepository.findAllByConcept_ConceptId(conceptId);
+        String generationId = null;
 
-        String generationId = leonardo.generateAndFetchImages(leonardo.putJsonPayload(settings, Prompt.findEnumById(conceptId), leonardo.init(multipartFile), breed));
+
+        if (conceptId == 1) {
+            String[] modelIds = {"modelId1_concept1", "modelId2_concept1", "modelId3_concept1"};
+            String selectedModelId = getRandomModelId(modelIds);
+            generationId = leonardo.generateAndFetchImages(leonardo.putJsonPayload(settings, Prompt.findEnumById(conceptId), leonardo.init(multipartFile), breed, selectedModelId));
+        } else if (conceptId == 2) {
+            String[] modelIds = {"modelId1_concept2", "modelId2_concept2", "modelId3_concept2"};
+            String selectedModelId = getRandomModelId(modelIds);
+            generationId = leonardo.generateAndFetchImages(leonardo.putJsonPayload(settings, Prompt.findEnumById(conceptId), leonardo.init(multipartFile), breed, selectedModelId));
+        }
+
+        //String generationId = leonardo.generateAndFetchImages(leonardo.putJsonPayload(settings, Prompt.findEnumById(conceptId), leonardo.init(multipartFile), breed));
         redisTemplate.opsForValue().set(generationId, AiDto.Data.builder().userId(userId).conceptId(conceptId).build());
     }
 
@@ -77,5 +91,11 @@ public class AiServiceImpl implements AiService {
             log.error("GenerationId를 찾을 수 없음! leonardoUrl : " + leonardoUrl);
             throw new Exception("url 에서 GenerationId 찾을 수 없음");
         }
+    }
+
+    private String getRandomModelId(String[] modelIds) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(modelIds.length);
+        return modelIds[randomIndex];
     }
 }

11c55485d9dff179f888b51810ba7f2ae2ecb981
successboy <chlgnlqls3@nate.com>
2023-11-06 13:12:50 +0900

    [BE]feat: 크리스마스 컨셉 추가

--- back/src/main/java/com/ssafy/petdio/service/AiServiceImpl.java
+++ back/src/main/java/com/ssafy/petdio/service/AiServiceImpl.java
@@ -6,14 +6,12 @@ import com.ssafy.petdio.model.dto.FcmDto.NotificationMessage;
 import com.ssafy.petdio.model.entity.Album;
 import com.ssafy.petdio.model.entity.Concept;
 import com.ssafy.petdio.model.entity.Setting;
-import com.ssafy.petdio.user.model.entity.User;
 import com.ssafy.petdio.repository.AlbumRepository;
 import com.ssafy.petdio.repository.SettingRepository;
+import com.ssafy.petdio.user.model.entity.User;
 import com.ssafy.petdio.user.repository.UserRepository;
 import com.ssafy.petdio.user.service.UserService;
 import com.ssafy.petdio.util.Leonardo;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,7 +20,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -73,6 +73,12 @@ public class AiServiceImpl implements AiService {
                     "d69c8273-6b17-4a30-a13e-d6637ae1c644","f1929ea3-b169-4c18-a16c-5d58b4292c69","1aa0f478-51be-4efd-94e8-76bfc8f533af"};
             String selectedModelId = getRandomModelId(modelIds);
             generationId = leonardo.generateAndFetchImages(leonardo.putJsonPayload(settings, Prompt.findEnumById(conceptId), leonardo.init(multipartFile), breed, selectedModelId));
+        } else if (conceptId == 5) {
+            String[] modelIds = {"e316348f-7773-490e-adcd-46757c738eb7", "ac614f96-1082-45bf-be9d-757f2d31c174",
+                    "1e60896f-3c26-4296-8ecc-53e2afecc132","5c232a9e-9061-4777-980a-ddc8e65647c6","2067ae52-33fd-4a82-bb92-c2c55e7d2786",
+                    "d69c8273-6b17-4a30-a13e-d6637ae1c644","f1929ea3-b169-4c18-a16c-5d58b4292c69","1aa0f478-51be-4efd-94e8-76bfc8f533af"};
+            String selectedModelId = getRandomModelId(modelIds);
+            generationId = leonardo.generateAndFetchImages(leonardo.putJsonPayload(settings, Prompt.findEnumById(conceptId),leonardo.init(multipartFile), breed, selectedModelId));
         }
 
         //String generationId = leonardo.generateAndFetchImages(leonardo.putJsonPayload(settings, Prompt.findEnumById(conceptId), leonardo.init(multipartFile), breed));

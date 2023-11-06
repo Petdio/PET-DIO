4022b6b45d3658768a112550195cac9faa492bd9
mj <aud19980518@gmail.com>
2023-11-06 10:05:52 +0900

    [BE] Refactor: 전체적으로 코드 수정2
    [BE] Refactor: 전체적으로 코드 수정2

--- back/src/main/java/com/ssafy/petdio/auth/oauth2/common/OAuthReqDto.java
+++ back/src/main/java/com/ssafy/petdio/auth/oauth2/common/OAuthReqDto.java
@@ -2,9 +2,11 @@ package com.ssafy.petdio.auth.oauth2.common;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
+@ToString
 public class OAuthReqDto {
     private String code;
     private String fcmToken;

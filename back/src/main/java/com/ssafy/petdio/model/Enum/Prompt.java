fd2202b69a92e7b1ab66b72dd07f54f81e300d17
Harvey Kwak <harvey970121@naver.com>
2023-11-06 15:31:10 +0900

    [BE] Feat: concept1 add  negative prompt

--- back/src/main/java/com/ssafy/petdio/model/Enum/Prompt.java
+++ back/src/main/java/com/ssafy/petdio/model/Enum/Prompt.java
@@ -20,7 +20,14 @@ public enum Prompt {
                     " 4K resolution, photorealistic rendering, using Cinema 4D,\n"
                     + "sticker, cartoon breed in the photo that provided sticker, all white background, Vermeer style, " +
                     "12K, high quality, HD, octane render, cinematic lighting, sticker format, sticker shape," +
-                    "Different every time, When making, Different every time I make a photo, not same", ""),
+                    "Different every time, When making, Different every time I make a photo, not same", "negative_prompt\", \"nude, nsfw, text, letters, too many feet, too many fingers, \" +\n"
+            + "\"(((2 heads))), duplicate, abstract, disfigured, deformed, toy, figure, framed, disfigured, bad art, \" +\n"
+            + "\"deformed, poorly drawn, extra limbs, weird colors, 2 heads, long neck, elongated body, cropped image, \" +\n"
+            + "\"out of frame, draft, deformed hands, twisted fingers, double image, malformed hands, \" +\n"
+            + "\"multiple heads, extra limb, ugly, poorly drawn hands, missing limb, cut-off, over satured, grain, lowères,\" +\n"
+            + "\" bad anatomy, poorly drawn face, mutation, mutated, floating limbs, disconnected limbs,\" +\n"
+            + "\" out of focus, long body, disgusting, extra fingers, groos proportions, missing arms, mutated hands, \" +\n"
+            + "\"cloned face, missing legs,"),
     HALLOWEEN(2L,
             "cute stickers, 8K Ultra HD, highly detailed, Detailed illustration of a cute breed enjoying Halloween, " +
                     "with lots of pumpkins in the background, magic hat, style cartoon, two-dimensional, planar vector, " +

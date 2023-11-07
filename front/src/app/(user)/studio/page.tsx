37c827b5bc44d8d48327b2898fa4bc44d9633bd2
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-11-07 10:35:05 +0900

    [FE] Test: 새로고침 방지 테스트

--- front/src/app/(user)/studio/page.tsx
+++ front/src/app/(user)/studio/page.tsx
@@ -1,7 +1,24 @@
+"use client";
 import ThemeList from "@/components/studio/theme-list/ThemeList";
 import PageTitle from "@/components/common/page-title/PageTitle";
+import { useEffect } from "react";
 
 export default function Studio() {
+  const preventClose = (e: BeforeUnloadEvent) => {
+    e.preventDefault();
+    e.returnValue = ""; //Chrome에서 동작하도록; deprecated
+  };
+
+  useEffect(() => {
+    (() => {
+      window.addEventListener("beforeunload", preventClose);
+    })();
+
+    return () => {
+      window.removeEventListener("beforeunload", preventClose);
+    };
+  }, []);
+
   return (
     <>
       <PageTitle pageTitleContent="사진관" subtitleContent="테마를 선택해요." />

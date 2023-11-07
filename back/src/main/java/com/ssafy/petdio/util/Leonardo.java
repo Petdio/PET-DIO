22742d5422de7cd2f65c011d1de37100741a02c0
successboy <chlgnlqls3@nate.com>
2023-11-07 17:14:41 +0900

    [BE]feat:이미지프롬프터테스트

--- back/src/main/java/com/ssafy/petdio/util/Leonardo.java
+++ back/src/main/java/com/ssafy/petdio/util/Leonardo.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -70,6 +71,8 @@ public class Leonardo {
         JSONObject fieldsJson = new JSONObject(fieldsString);
 
         String urlUploadImage = jsonResponse.getJSONObject("uploadInitImage").getString("url");
+        String imageId = jsonResponse.getJSONObject("uploadInitImage").getString("id");
+
 
         System.out.println(jsonResponse.getJSONObject("uploadInitImage"));
 
@@ -122,6 +125,8 @@ public class Leonardo {
         generationPayload.put("negative_prompt", prompt.getNegativePrompt());
         generationPayload.put("init_image_id", imageId);
         generationPayload.put("modelId", selectedModelId);
+        List<String> imagePrompts = new ArrayList<>();
+        generationPayload.put("imagePrompts", imagePrompts);
 
         return generationPayload;
     }

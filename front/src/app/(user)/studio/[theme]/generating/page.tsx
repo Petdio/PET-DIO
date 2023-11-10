b498ce6999cb6c3c9d77f9169a240912067ebef3
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-11-10 17:59:26 +0900

    [FE] Fix: fcm 토큰 있을 경우의 코드 추가

--- front/src/app/(user)/studio/[theme]/generating/page.tsx
+++ front/src/app/(user)/studio/[theme]/generating/page.tsx
@@ -1,18 +1,18 @@
-'use client';
-import { useState, useEffect } from 'react';
-import { LinearProgress, Box, Typography } from '@mui/material';
-import CheckIcon from '@mui/icons-material/Check';
-import { useRouter } from 'next/navigation';
-import { initializeApp } from 'firebase/app';
-import { getMessaging, onMessage } from 'firebase/messaging';
-import { theme } from '@/styles/ThemeRegistry';
+"use client";
+import { useState, useEffect } from "react";
+import { LinearProgress, Box, Typography } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import { useRouter } from "next/navigation";
+import { initializeApp } from "firebase/app";
+import { getMessaging, onMessage } from "firebase/messaging";
+import { theme } from "@/styles/ThemeRegistry";
 
 const loadingMessageArr = [
-  '사진과 일치하는 품종을 입력해야 원하는 이미지를 얻을 수 있어요.',
-  '생성된 이미지를 저장하거나 다른 사람들과 공유해보세요!',
-  '앨범 페이지에서 원하는 테마만 필터링할 수 있어요.',
-  '반려동물 사진은 정방향으로, 흔들리지 않게, 얼굴이 잘 나오도록 찍어주세요.',
-  '이미지 생성 후 설문조사를 완료하시면 코인을 추가로 받을 수 있어요.',
+  "사진과 일치하는 품종을 입력해야 원하는 이미지를 얻을 수 있어요.",
+  "생성된 이미지를 저장하거나 다른 사람들과 공유해보세요!",
+  "앨범 페이지에서 원하는 테마만 필터링할 수 있어요.",
+  "반려동물 사진은 정방향으로, 흔들리지 않게, 얼굴이 잘 나오도록 찍어주세요.",
+  "이미지 생성 후 설문조사를 완료하시면 코인을 추가로 받을 수 있어요.",
 ];
 
 export default function Generating() {
@@ -31,6 +31,31 @@ export default function Generating() {
   }
 
   useEffect(() => {
+    const firebaseApp = initializeApp({
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+    });
+
+    const messaging = getMessaging(firebaseApp);
+
+    onMessage(messaging, (payload) => {
+      console.log("Message received. ", payload.notification?.image);
+      const imageKey = payload.notification?.image?.match(/\/([^/]+)\.jpg$/);
+
+      if (imageKey) {
+        setShowComponent(false);
+        setTimeout(() => {
+          router.push(`/studio/result?img=${imageKey[1]}`);
+        }, 4000);
+      } else {
+        console.log("No match found");
+      }
+    });
     const eventSource = new EventSource(
       `${
         process.env.NEXT_PUBLIC_API_URL
@@ -61,16 +86,16 @@ export default function Generating() {
   return (
     <Box
       sx={{
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <Box
         sx={{
-          width: '70%',
-          justifyContent: 'space-around',
+          width: "70%",
+          justifyContent: "space-around",
         }}
       >
         {showComponent ? (
@@ -86,10 +111,10 @@ export default function Generating() {
               variant="body1"
               color={theme.palette.common.black}
               sx={{
-                textAlign: 'center',
-                mt: '0.5rem',
-                mb: '20px',
-                wordBreak: 'keep-all',
+                textAlign: "center",
+                mt: "0.5rem",
+                mb: "20px",
+                wordBreak: "keep-all",
               }}
             >
               {loadingMessageArr[loadingMessageIdx]}
@@ -101,14 +126,11 @@ export default function Generating() {
             <Typography
               variant="body1"
               color="black"
-              sx={{ textAlign: 'center', mb: '20px' }}
+              sx={{ textAlign: "center", mb: "20px" }}
             >
               이미지 생성 완료!
             </Typography>
-            <CheckIcon
-              color="primary"
-              sx={{ width: '100%' }}
-            />
+            <CheckIcon color="primary" sx={{ width: "100%" }} />
           </>
         )}
       </Box>

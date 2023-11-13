7885217927116dcaadb63ac7bbc7af0060c08092
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-11-13 10:49:21 +0900

    [FE] Refactor: 화면 렌더링 후 필터 보이게 함

--- front/src/components/album/theme-select/theme-select-button/ThemeSelectButton.tsx
+++ front/src/components/album/theme-select/theme-select-button/ThemeSelectButton.tsx
@@ -14,6 +14,7 @@ function ThemeSelectButton({ isFiltered, onClick, disabled }: Props) {
   const selectThemeContent = "테마 선택";
   const cancelThemeContent = "선택 취소";
 
+  const [showFab, setShowFab] = useState(false);
   const [windowWidth, setWindowWidth] = useState(0);
   const rightPosition =
     windowWidth >= 480 ? `calc((100vw - 480px) / 2 + 1rem)` : "1rem";
@@ -29,11 +30,17 @@ function ThemeSelectButton({ isFiltered, onClick, disabled }: Props) {
     // 이벤트 리스너 추가
     window.addEventListener("resize", handleResize);
 
+    setShowFab(true);
+
     // 언마운트 시에 이벤트 리스너 제거
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []); // 빈 배열은 컴포넌트가 마운트될 때 한 번만 실행
+
+  if (!showFab) {
+    return <></>;
+  }
 
   return (
     <Fab

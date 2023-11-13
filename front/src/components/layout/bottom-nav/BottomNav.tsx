9926b1449a90facee571d18eddbaf8b2beea7190
serenecarp <spmjoon516@gmail.com>
2023-11-13 17:57:02 +0900

    [FE] Feat: 바텀내비게이션 추가

--- front/src/components/layout/bottom-nav/BottomNav.tsx
+++ front/src/components/layout/bottom-nav/BottomNav.tsx
@@ -1,10 +1,11 @@
-'use client';
+"use client";
 
-import { usePathname } from 'next/navigation';
-import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import CameraIcon from '@mui/icons-material/Camera';
-import CollectionsIcon from '@mui/icons-material/Collections';
-import { theme } from '@/styles/ThemeRegistry';
+import { usePathname } from "next/navigation";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import PaletteIcon from "@mui/icons-material/Palette";
+import CameraIcon from "@mui/icons-material/Camera";
+import CollectionsIcon from "@mui/icons-material/Collections";
+import { theme } from "@/styles/ThemeRegistry";
 
 interface Props {}
 
@@ -12,33 +13,39 @@ function BottomNav() {
   let activeNum = 0;
   let pathname = usePathname();
   switch (pathname) {
-    case '/studio':
+    case "/studio":
       activeNum = 0;
       break;
-    case '/album':
+    case "/album":
       activeNum = 1;
       break;
   }
   return (
     <BottomNavigation
       sx={{
-        display: 'flex',
-        justifyContent: 'space-evenly',
-        width: '100%',
-        height: '72px',
-        position: 'fixed',
+        display: "flex",
+        justifyContent: "space-evenly",
+        width: "100%",
+        height: "72px",
+        position: "fixed",
         bottom: 0,
-        maxWidth: '480px',
+        maxWidth: "480px",
         // 컬러코드 글로벌에서 가져올 것
-        borderTop: '1px solid #d9d9d9',
+        borderTop: "1px solid #d9d9d9",
       }}
       value={activeNum}
       showLabels
     >
       <BottomNavigationAction
         href="/studio"
+        icon={<PaletteIcon />}
+        label="캐주얼"
+        disableRipple
+      />
+      <BottomNavigationAction
+        href="/ai-studio"
         icon={<CameraIcon />}
-        label="사진관"
+        label="스튜디오"
         disableRipple
       />
       <BottomNavigationAction

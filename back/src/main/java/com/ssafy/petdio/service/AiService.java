5214cb5a2a0ef71ac52a5d67d60cbb6119e191ed
successboy <chlgnlqls3@nate.com>
2023-11-14 17:06:16 +0900

    feat:레오나르도 모델 사용 코드추가

--- back/src/main/java/com/ssafy/petdio/service/AiService.java
+++ back/src/main/java/com/ssafy/petdio/service/AiService.java
@@ -7,4 +7,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public interface AiService {
     String makeAiImage(Long conceptId, MultipartFile multipartFile, String breed, Long userId) throws IOException;
     void webhookUrlCheck(String url) throws Exception;
+
+    String makerealPhotoImage(Long conceptId, String modelId, Long userId) throws  IOException;
+
 }

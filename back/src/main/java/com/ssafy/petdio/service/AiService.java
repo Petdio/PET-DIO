83ccbe95355b0c2a6b5745810a24f790d56059ed
mj <aud19980518@gmail.com>
2023-11-14 16:11:57 +0900

    [BE] Refactor: 코드 수정

--- back/src/main/java/com/ssafy/petdio/service/AiService.java
+++ back/src/main/java/com/ssafy/petdio/service/AiService.java
@@ -6,8 +6,5 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public interface AiService {
     String makeAiImage(Long conceptId, MultipartFile multipartFile, String breed, Long userId) throws IOException;
-
-    void getImage(String leonardoUrl) throws Exception;
-
     void webhookUrlCheck(String url) throws Exception;
 }

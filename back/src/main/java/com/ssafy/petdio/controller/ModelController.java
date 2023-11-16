e26a82fb1f942d05a147d8ee117d5c1e4f7f77ff
novventa <novventa@kakao.com>
2023-11-16 15:39:35 +0900

    [BE] Refactor: 모델컨트롤러 수정

--- back/src/main/java/com/ssafy/petdio/controller/ModelController.java
+++ back/src/main/java/com/ssafy/petdio/controller/ModelController.java
@@ -24,7 +24,7 @@ public class ModelController {
 
     @PostMapping("/train")
     public ResponseEntity createDataset(@RequestParam("datasetName") String datasetName,
-                                        @RequestParam(value = "files") List<MultipartFile> files,
+                                        @RequestParam("files") List<MultipartFile> files,
                                         @RequestParam("breed") String breed,
                                         Authentication authentication)
     {
